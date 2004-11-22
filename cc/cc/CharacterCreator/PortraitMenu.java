80ab73b6ee94382ba0633046b767cdd0797af73f
judd <judd>
2004-11-22 01:57:09 +0000

    Strip m,l,t,s,h from the end of portrait names.

--- cc/cc/CharacterCreator/PortraitMenu.java
+++ cc/cc/CharacterCreator/PortraitMenu.java
@@ -89,7 +89,9 @@ public class PortraitMenu extends javax.swing.JFrame {
 					CurrentPortrait.setIcon(icon);
                     BICPortraitname = baseFilename.substring(0, baseFilename.length() - 4);
                     InfoText.setText(PORTRAIT.substring(PORTRAIT.lastIndexOf(FileDelim) + 1));
-                    if(BICPortraitname.endsWith("m")) {
+                    if(BICPortraitname.endsWith("m") || BICPortraitname.endsWith("l")
+							|| BICPortraitname.endsWith("h") || BICPortraitname.endsWith("s")
+							|| BICPortraitname.endsWith("t")) {
                         BICPortraitname = BICPortraitname.substring(0, BICPortraitname.length() - 1);
                     }
                     OKButton.setEnabled(true);

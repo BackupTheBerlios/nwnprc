d9691ae9402b6d2e35d1267a8f530ae6eee98133
primogenitor <primogenitor>
2004-12-24 13:43:32 +0000

    fixed it not cleaning psionicobjs

--- nwnprc/clean.bat
+++ nwnprc/clean.bat
@@ -3,6 +3,7 @@ del /q epicspellobjs\*.*
 del /q objs\*.*
 del /q raceobjs\*.*
 del /q spellobjs\*.*
+del /q psionicobjs\*.*
 del /q *.temp
 del /q CompiledResources\*.hak
 del /q CompiledResources\*.erf

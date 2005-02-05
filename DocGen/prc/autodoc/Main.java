e9571d9237ede8108734a951d5a3f46f8f461e22
ornedan <ornedan>
2005-02-05 12:17:53 +0000

    *** empty log message ***

---
+++ DocGen/prc/autodoc/Main.java
@@ -0,0 +1,10 @@
+package prc.autodoc;
+
+import java.io.*;
+import java.util.*;
+
+public class Main{
+
+	public static final boolean verbose = true;
+	public static final Spinner spinner = new Spinner();
+}

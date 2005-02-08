d59c5fa419812e78706554efeb23097f2ca64e10
ornedan <ornedan>
2005-02-08 19:08:12 +0000

    *** empty log message ***

---
+++ DocGen/twoDAcomp.java
@@ -0,0 +1,32 @@
+import prc.autodoc.*;
+
+import java.io.*;
+import java.util.*;
+
+public class twoDAcomp{
+	
+	public static void main(String[] args) throws Exception{
+		File[] prc2das = new File(".\\prc2das\\").listFiles();
+		File[] nor2das = new File(".\\2das\\").listFiles();
+		
+		
+		HashMap<String, File> cont = new HashMap<String, File>();
+		
+		for(File elem : nor2das) cont.put(elem.getName().toLowerCase(), elem);
+		
+		for(File file1 : prc2das){
+			File file2 = cont.get(file1.getName().toLowerCase());
+			if(file2 == null) continue;
+			try{
+				Data_2da data1 = new Data_2da(file1.getPath());
+				Data_2da data2 = new Data_2da(file2.getPath());
+				
+				Data_2da.doComparison(data1, data2);
+			}catch(Exception e){
+				System.out.println("Exception");
+				System.err.println(e);
+			}
+		}
+		
+	}
+}

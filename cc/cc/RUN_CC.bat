113c47ce2e83b475257109b25954670643799a36
ornedan <ornedan>
2005-02-20 14:33:00 +0000

    Rewritten. Added memory controls to give the VM 150Mb to work in.

--- cc/cc/RUN_CC.bat
+++ cc/cc/RUN_CC.bat
@@ -1,6 +1,6 @@
+@REM Runs the PRC character creator. Use of this is required due to the memory requirements
+@REM caused by the 2das in 2.3+ being greater than what Java VM normally assigns.
 
-SET PATH=D:\Program Files\Java\jdk1.5.0\bin
+java -Xmx150M -Xms150M -jar CC.jar
 
-javaw -jar CC.jar
-
-PAUSE
+@pause

6a5b0a36bbdba324a4aad2e8c92f1956bf1472a9
primogenitor <primogenitor>
2005-05-07 16:45:52 +0000

    split the installation manual into separate parts

---
+++ DocGen/copyandbuild.bat
@@ -0,0 +1,9 @@
+copy ..\prc\2das\*.2da 2da\*.2da
+copy ..\prc\race2das\*.2da 2da\*.2da
+copy ..\prc\craft2das\*.2da 2da\*.2da
+copy ..\prc\tlk\*.tlk tlk\*.tlk
+xcopy /iey "Main Manual Files" manual
+
+nmake -NOLOGO
+
+nmake -NOLOGO run_icons

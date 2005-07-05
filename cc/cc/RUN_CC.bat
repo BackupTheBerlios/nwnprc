d5d61c7b533a32114bae39d5d1cca92b7b06a484
ornedan <ornedan>
2005-07-05 19:40:37 +0000

    Some more memory & automatic logging

--- cc/cc/RUN_CC.bat
+++ cc/cc/RUN_CC.bat
@@ -1,6 +1,5 @@
 @REM Runs the PRC character creator. Use of this is required due to the memory requirements
 @REM caused by the 2das in 2.3+ being greater than what Java VM normally assigns.
 
-java -Xmx150M -Xms150M -jar CC.jar
+java -Xmx200M -Xms200M -jar CC.jar 1> cc.log
 
-@pause

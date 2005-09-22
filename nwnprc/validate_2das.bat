01ef6a6e8c95da2bc61553d29fd860b9f94bb195
ornedan <ornedan>
2005-09-22 08:04:19 +0000

    *** empty log message ***

--- nwnprc/validate_2das.bat
+++ nwnprc/validate_2das.bat
@@ -1,2 +1,2 @@
-@dir /b /s 2das\*.2da | tools\ssed -R "$! {s/$/\"/g};s/^/\"/g" | java -Xmx150m -jar tools\prc.jar 2da -n - 2>&1
+@dir /b /s 2das\*.2da | tools\ssed -R "$! {s/^/\"/g};s/$/\"/g" | java -Xmx150m -jar tools\prc.jar 2da -n - 2>&1
 @pause

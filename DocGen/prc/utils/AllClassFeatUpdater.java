af8be3780738019ca250e1ff60ea8066606b2bf9
ornedan <ornedan>
2005-10-06 17:57:10 +0000

    Added capacity to crop duplicate feats from being added by the update

--- DocGen/prc/utils/AllClassFeatUpdater.java
+++ DocGen/prc/utils/AllClassFeatUpdater.java
@@ -17,13 +17,15 @@ import prc.autodoc.Data_2da;
  * @author Ornedan
  */
 public class AllClassFeatUpdater {
+	
+	private static boolean cropduplicates = false;
 
 	/**
 	 * Ye olde main method.
 	 * Takes as parameters the path of the 2da to use as update template and
 	 * 
 	 * 
-	 * @param args Parameters received
+	 * @param args [--help] | [-c] pathtoupdate2da targetpath+
 	 */
 	public static void main(String[] args) {
 		if(args.length == 0) readMe();
@@ -38,8 +40,11 @@ public class AllClassFeatUpdater {
 				else{
 					for(char c : param.substring(1).toCharArray()){
 						switch(c){
+						case 'c':
+							cropduplicates = true;
+							break;
 						default:
-							System.out.println("Unknown parameter: " + c + "!");
+							System.out.println("Unknown parameter: " + c);
 							readMe();
 						}
 					}
@@ -121,7 +126,7 @@ public class AllClassFeatUpdater {
 		}
 	}
 	
-	private static void update2da(Data_2da source, Data_2da target, String path) throws IOException{
+	private static void update2da(Data_2da updateSource, Data_2da target, String path) throws IOException{
 		// Find the beginning of replaceable area
 		int beginRow = -1, i = 0;
 		while(beginRow == -1 && i < target.getEntryCount()){
@@ -140,27 +145,55 @@ public class AllClassFeatUpdater {
 					target.removeRow(beginRow);
 			}
 			
-			for(i = 0; i < source.getEntryCount(); i++){
-				target.insertRow(beginRow + i, source.getRow(i));
+			// Handle cropping, if it is enabled
+			if(cropduplicates){
+				// Make a copy of the update source, which can then be cropped
+				updateSource = (Data_2da)updateSource.clone();
+				cropDuplicates(updateSource, target);
 			}
 			
-			//System.out.println(target);
+			for(i = 0; i < updateSource.getEntryCount(); i++){
+				target.insertRow(beginRow + i, updateSource.getRow(i));
+			}
+			
+			//System.out.println(target); System.exit(0);
 			target.save2da(path, true, true);
 		}
 	}
 	
+	private static void cropDuplicates(Data_2da toCrop, Data_2da source){
+		// Load the featIDs from source to a hashset
+		HashSet<Integer> featIDs = new HashSet<Integer>();
+		
+		for(int i = 0; i < source.getEntryCount(); i++){
+			try{
+				featIDs.add(Integer.parseInt(source.getEntry("FeatIndex", i)));
+			}catch(NumberFormatException nfe){/* Ignore empty rows */}
+		}
+		
+		// Loop over the crop target, removing rows whose FeatIndex is present in the source
+		for(int i = toCrop.getEntryCount() - 1; i >= 0 ; i--){
+			try{
+				if(featIDs.contains(Integer.parseInt(toCrop.getEntry("FeatIndex", i))))
+					toCrop.removeRow(i);
+			}catch(NumberFormatException nfe){/* Ignore empty rows */}
+		}
+			
+	}
+	
 	private static void readMe(){
 		System.out.println("Usage:\n"+
-                           "  [--help] | pathtoupdate2da targetpath+\n"+
+                           "  [--help] | [-c] pathtoupdate2da targetpath+\n"+
                            "\n" +
-                           "pathtoupdate2da  path of the cls_feat_allBaseClasses.2da\n"+
-                           "targetpath       the path of a directory containing the cls_feat_*.2das to update\n" +
-                           "                 or directly the path of a cls_feat_*.2da"+
-                           "\n"+
+                           " pathtoupdate2da  path of the cls_feat_allBaseClasses.2da\n"+
+                           " targetpath       the path of a directory containing the cls_feat_*.2das to update\n" +
+                           "                  or directly the path of a cls_feat_*.2da\n"+
+                           "\n" +
+                           "  -c      crop rows from the update source that contain featIDs present elsewhere\n" +
+                           "          in the target than in the updateable area\n"+
                            "\n"+
                            "  --help  prints this text\n"
                 );
 		System.exit(0);
 	}
-
 }

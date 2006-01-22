044422d7dcd252ae8c0da86d6b551aa106bc301c
primogenitor <primogenitor>
2006-01-22 18:50:28 +0000

    massive fixes additions. Should now be mostly complete except for tlk entries.

--- DocGen/prc/utils/SpellbookMaker.java
+++ DocGen/prc/utils/SpellbookMaker.java
@@ -15,12 +15,17 @@ public final class SpellbookMaker{
 	private static int feat2daRow   = 0;
 	private static int iprp_feats2daRow   = 0;
 	private static int tlkRow       = 0;
+	private static int classSpellRow= 0;
 	private static Data_2da classes2da;
 	private static Data_2da spells2da;
 	private static Data_2da feat2da;
 	private static Data_2da iprp_feats2da;
 	private static Data_TLK customtlk;
 	private static Data_TLK dialogtlk;
+	private static Data_2da classSpell2da;
+	private static String[] spellLabels;
+
+	private static int MAGIC_TLK = 16777216;
 
 	/**
 	 * The main method, as usual.
@@ -37,18 +42,20 @@ public final class SpellbookMaker{
 		iprp_feats2da = Data_2da.load2da("2das\\iprp_feats.2da", true);
 		customtlk = new Data_TLK("tlk\\prc_consortium.tlk");
 		dialogtlk = new Data_TLK("tlk\\dialog.tlk");
+		spellLabels = spells2da.getLabels();
 		//get the start/end rows for each file for the reserved blocks
 		getFirstSpells2daRow();
 		getFirstFeat2daRow();
-		getFirstIPRPFeat2daRow();
-		getFirsttlkRow();
+		getFirstIPRPFeats2daRow();
+		getFirstTlkRow();
 		System.out.println("First free spells.2da row is "+spells2daRow);
 		System.out.println("First free feat.2da row is "+feat2daRow);
+		System.out.println("First free iprp_feats.2da row is "+iprp_feats2daRow);
 		System.out.println("First free tlk row is "+tlkRow);
 		//now process each class in turn
-		for(int i = 0; i<255;i++){
+		for(int classRow = 0; classRow<255; classRow++){
 			//the feat file is the root of the file naming layout
-			String classfilename = classes2da.getEntry("FeatsTable", i);
+			String classfilename = classes2da.getEntry("FeatsTable", classRow);
 			//check its a real class not padding
 			if(classfilename != null
 				&& classfilename != "****"
@@ -63,17 +70,43 @@ public final class SpellbookMaker{
 					Data_2da classCoreSpell2da = Data_2da.load2da("2das\\"+classCoreFilename+".2da", true);
 					//get the new filename
 					String classSpellFilename = "cls_spell_"+classfilename;
-					Data_2da classSpell2da = Data_2da.load2da("2das\\"+classSpellFilename+".2da", true);
+					classSpell2da = Data_2da.load2da("2das\\"+classSpellFilename+".2da", true);
 					//get the class name
-					String className = getCheckedTlkEntry(classes2da.getBiowareEntryAsInt("Name", i))+" ";
+					String className = getCheckedTlkEntry(classes2da.getBiowareEntryAsInt("Name", classRow))+" ";
+					String classLabel = classes2da.getEntry("Label", classRow)+"_";
+					//get the maximum spell level
+					int maxLevel = 0;
+					for(int row = 0; row < classCoreSpell2da.getEntryCount(); row ++) {
+						int tempLevel = classCoreSpell2da.getBiowareEntryAsInt("Level", row);
+						if(tempLevel>maxLevel){
+							maxLevel = tempLevel;
+						}
+					}
+					//clear the old cls_spell_*.2da file
+					String[] originalClassSpellRow = classSpell2da.getLabels();
+					for(int row = 0; row < classSpell2da.getEntryCount(); row ++) {
+						for(int i=0; i < originalClassSpellRow.length; i++){
+							classSpell2da.setEntry(originalClassSpellRow[i], row, "****");
+						}
+					}
+					classSpellRow = 0;//number of rows in the cls_spell_*.2da file
 					//loop over all the spells
 					for(int row = 0; row < classCoreSpell2da.getEntryCount(); row ++) {
 						//get the real spellID
 						int spellID = classCoreSpell2da.getBiowareEntryAsInt("SpellID", row);
 						//get the name of the spell
 						String spellName = getCheckedTlkEntry(spells2da.getBiowareEntryAsInt("Name", spellID));
+						//get the label of the spell
+						String spellLabel = spells2da.getEntry("Label", spellID);
+						//get the level of the spell
+						int spellLevel = classCoreSpell2da.getBiowareEntryAsInt("Level", row);
 						//get the metamagic reference to know what types work
 						int metamagic = spells2da.getBiowareEntryAsInt("Metamagic", spellID);
+						if(metamagic == 0)
+							System.out.println("Check metamagic for spell "+spellID);
+
+						//need to handle subradials here too
+
 						//now loop over the metamagic varients
 						//-1 represents no metamagic
 						for(int metamagicNo = -1; metamagicNo < 6; metamagicNo++){
@@ -94,42 +127,255 @@ public final class SpellbookMaker{
                             if((metamagic & hexValue) != 0
                             	|| (hexValue == 0)){
 								String spellNameMetamagic = "";
+								String spellLabelMetamagic = "";
+								String metaScript = "";
+								int metamagicLevel = 0;
+								String metamagicFeat = "****";
 								if(metamagicNo == -1){
 									spellNameMetamagic = "";
+									spellLabelMetamagic = "";
+									metaScript = "sp";
+									metamagicLevel = 0;
+									metamagicFeat = "****";
 								} else if(metamagicNo == 0){
 									spellNameMetamagic = "Empowered ";
+									spellLabelMetamagic = "Empowered_";
+									metaScript = "em";
+									metamagicLevel = 2;
+									metamagicFeat = "11";
 								} else if(metamagicNo == 1){
 									spellNameMetamagic = "Extended ";
+									spellLabelMetamagic = "Exteneded_";
+									metaScript = "ex";
+									metamagicLevel = 1;
+									metamagicFeat = "12";
 								} else if(metamagicNo == 2){
 									spellNameMetamagic = "Maximized ";
+									spellLabelMetamagic = "Maximized_";
+									metaScript = "ma";
+									metamagicLevel = 3;
+									metamagicFeat = "25";
 								} else if(metamagicNo == 3){
 									spellNameMetamagic = "Quickened ";
+									spellLabelMetamagic = "Quickened_";
+									metaScript = "qu";
+									metamagicLevel = 4;
+									metamagicFeat = "29";
 								} else if(metamagicNo == 4){
 									spellNameMetamagic = "Silent ";
+									spellLabelMetamagic = "Silent_";
+									metaScript = "si";
+									metamagicLevel = 1;
+									metamagicFeat = "33";
 								} else if(metamagicNo == 5){
 									spellNameMetamagic = "Still ";
+									spellLabelMetamagic = "Still_";
+									metaScript = "st";
+									metamagicLevel = 1;
+									metamagicFeat = "37";
 								}
-								String name = className+spellNameMetamagic+spellName;
-								System.out.println(name);
-							}
-						}
-
-					}
+								//check if the metamagic adjusted level is less than the maximum level
+								if((metamagicLevel+spellLevel) <= maxLevel){
+									//assemble the name
+									String name = className+spellNameMetamagic+spellName;
+									//assemble the label
+									String label = classLabel+spellLabelMetamagic+spellLabel;
+									//debug printout
+									//System.out.println(name+" : "+label);
+									addNewSpellbookData(spellID,
+														classfilename,
+														metaScript,
+														metamagicNo,
+														metamagicLevel,
+														metamagicFeat,
+														spellLevel,
+														name,
+														label,
+														0);
+								}//end of level check
+							}//end of metamamgic check
+						}//end of metamagic loop
+					}//end of cls_spells_*_core.2da loop
+					//save the new cls_spell_*.2da file
+					classSpell2da.save2da("2das", true, true);
 				} else {
 					//System.out.println(classfilename+" does not exist.");
 				}
 			}
 		}
+		//now resave the files we opened
+		spells2da.save2da("2das", true, true);
+		feat2da.save2da("2das", true, true);
+		iprp_feats2da.save2da("2das", true, true);
+	}
+
+	private static void addNewSpellbookData(int spellID,
+											String classfilename,
+											String metaScript,
+											int metamagicNo,
+											int metamagicLevel,
+											String metamagicFeat,
+											int spellLevel,
+											String name,
+											String label,
+											int subradialMaster){
+		//set the next tlk line to the name
+			//need to implement tlk writing
+		//copy the original spells.2da line to the next free spells.2da line
+		String[] originalSpellRow = spells2da.getRow(spellID);
+		for(int i=0; i<originalSpellRow.length; i++){
+			spells2da.setEntry(spellLabels[i], spells2daRow, spells2da.getEntry(spellLabels[i], spellID));
+		}
+		//change the ImpactScript
+		String script = "prc_"+classfilename+"_"+metaScript+"_gen";
+		spells2da.setEntry("ImpactScript", spells2daRow, script);
+		//change the Label
+		spells2da.setEntry("Label", spells2daRow, label);
+		//change the Name
+		spells2da.setEntry("Name", spells2daRow, Integer.toString(tlkRow+MAGIC_TLK));
+		//make it point to the new feat.2da line that will be added soon
+		spells2da.setEntry("FeatID", spells2daRow, Integer.toString(feat2daRow));
+		//if quickened, set oconjuring/casting duration to zero
+		if(metamagicNo == 3){
+			spells2da.setEntry("ConjTime", spells2daRow, "0");
+			spells2da.setEntry("CastTime", spells2daRow, "0");
+		}
+		//if silenced, set it to no vocals
+		if(metamagicNo == 4){
+			spells2da.setEntry("ConjSoundVFX", spells2daRow, "****");
+			spells2da.setEntry("ConjSoundMale", spells2daRow, "****");
+			spells2da.setEntry("ConjSoundFemale", spells2daRow, "****");
+			spells2da.setEntry("CastSound", spells2daRow, "****");
+		}
+		//if stilled, set it to no casting animations
+		if(metamagicNo == 5){
+			spells2da.setEntry("CastAnim", spells2daRow, "****");
+			spells2da.setEntry("ConjAnim", spells2daRow, "****");
+		}
+		//set the level to the correct value, including metamagic
+		spells2da.setEntry("Innate", spells2daRow, Integer.toString(metamagicLevel+spellLevel));
+		//clear class levels
+		spells2da.setEntry("Bard", spells2daRow, "****");
+		spells2da.setEntry("Cleric", spells2daRow, "****");
+		spells2da.setEntry("Druid", spells2daRow, "****");
+		spells2da.setEntry("Paladin", spells2daRow, "****");
+		spells2da.setEntry("Ranger", spells2daRow, "****");
+		spells2da.setEntry("Wiz_Sorc", spells2daRow, "****");
+		//set subradial information later on
+		//set subradial master, if applicable
+		if(subradialMaster != 0){
+			spells2da.setEntry("Master", spells2daRow, Integer.toString(subradialMaster));
+		} else {
+			spells2da.setEntry("Master", spells2daRow, "****");
+		}
+
+		//add a feat.2da line
+		//make it point to the new spells.2da line
+		feat2da.setEntry("SPELLID", feat2daRow, Integer.toString(spells2daRow));
+		//change the Name
+		feat2da.setEntry("FEAT", feat2daRow, Integer.toString(tlkRow+MAGIC_TLK));
+		//change the Label
+		feat2da.setEntry("LABEL", feat2daRow, label);
+		//change the description
+		feat2da.setEntry("DESCRIPTION", feat2daRow, spells2da.getEntry("SpellDesc", spells2daRow));
+		//change the icon
+		feat2da.setEntry("ICON", feat2daRow, spells2da.getEntry("IconResRef", spells2daRow));
+		//if spell is hostile, make feat hostile
+		if(spells2da.getEntry("HostileSetting", spellID).equals("1")){
+			feat2da.setEntry("HostileFeat", feat2daRow, "1");
+		} else {
+			feat2da.setEntry("HostileFeat", feat2daRow, "0");
+		}
+		//set the category to the same as the spell
+		feat2da.setEntry("CATEGORY", feat2daRow, spells2da.getEntry("Category", spells2daRow));
+
+
+		//add an iprp_feats.2da line
+		//set its label
+		iprp_feats2da.setEntry("Label", iprp_feats2daRow, label);
+		//set its name
+		iprp_feats2da.setEntry("Name", iprp_feats2daRow, Integer.toString(tlkRow+MAGIC_TLK));
+		//make it point to the new feat.2da line
+		iprp_feats2da.setEntry("FeatIndex", iprp_feats2daRow, Integer.toString(feat2daRow));
+		//set its cost to 0.0
+		iprp_feats2da.setEntry("Cost", iprp_feats2daRow, "0.0");
+
+		//add a cls_spell_*.2da line if needed
+		if(classSpellRow < classSpell2da.getEntryCount()){
+				classSpell2da.appendRow();
+		}
+		//set its label
+		classSpell2da.setEntry("Label", classSpellRow, label);
+		//make it point to the new spells.2da
+		classSpell2da.setEntry("SpellID", classSpellRow, Integer.toString(spells2daRow));
+		//make it point to the old spells.2da
+		classSpell2da.setEntry("RealSpellID", classSpellRow, Integer.toString(spellID));
+		//if its a subradial, dont do this
+		if(subradialMaster == 0){
+			//make it point to the new feat.2da
+			classSpell2da.setEntry("FeatID", classSpellRow, Integer.toString(feat2daRow));
+			//make it point to the new iprp_feats.2da
+			classSpell2da.setEntry("IPFeatID", classSpellRow, Integer.toString(iprp_feats2daRow));
+			//add the metamagic checks
+			classSpell2da.setEntry("ReqFeat", classSpellRow, metamagicFeat);
+			//set its level
+			classSpell2da.setEntry("Level", classSpellRow, Integer.toString(metamagicLevel+spellLevel));
+		} else {
+			//make it point to the new feat.2da
+			classSpell2da.setEntry("FeatID", classSpellRow, "****");
+			//make it point to the new iprp_feats.2da
+			classSpell2da.setEntry("IPFeatID", classSpellRow, "****");
+			//add the metamagic checks
+			classSpell2da.setEntry("ReqFeat", classSpellRow, "****");
+			//set its level
+			classSpell2da.setEntry("Level", classSpellRow, "****");
+		}
+
+		//subradial checks
+		for(int subradial = 1; subradial <= 5; subradial++){
+			if(spells2da.getBiowareEntryAsInt("SubRadSpell"+subradial, spells2daRow) != 0){
+				spells2da.setEntry("SubRadSpell"+subradial, spells2daRow, Integer.toString(spells2daRow+subradial));
+			} else {
+				spells2da.setEntry("SubRadSpell"+subradial, spells2daRow, "****");
+			}
+		}
+		subradialMaster = spells2daRow;
+
+		//move to next file lines
+		getNextSpells2daRow();
+		getNextFeat2daRow();
+		getNextIPRPFeats2daRow();
+		getNextTlkRow();
+		classSpellRow++;
+
+		//add subradial spells
+		for(int subradial = 1; subradial <= 5; subradial++){
+			if(spells2da.getBiowareEntryAsInt("SubRadSpell"+subradial, subradialMaster) != 0){
+				addNewSpellbookData(spells2da.getBiowareEntryAsInt("SubRadSpell"+subradial, spellID),
+									classfilename,
+									metaScript,
+									metamagicNo,
+									metamagicLevel,
+									metamagicFeat,
+									spellLevel,
+									name,
+									label,
+									subradialMaster);
+			}
+		}
+
 	}
 
 	private static void getFirstSpells2daRow(){
 		while(!spells2da.getEntry("Label", spells2daRow).equals("####START_OF_NEW_SPELLBOOK_RESERVE")){
 			spells2daRow++;
 		}
+		spells2daRow++;
 	}
 	private static void getNextSpells2daRow(){
-		while(spells2da.getEntry("Label", spells2daRow).equals("####END_OF_NEW_SPELLBOOK_RESERVE")){
-			spells2daRow++;
+		spells2daRow++;
+		if(spells2da.getEntry("Label", spells2daRow).equals("####END_OF_NEW_SPELLBOOK_RESERVE")){
+			getFirstSpells2daRow();
 		}
 	}
 
@@ -137,37 +383,43 @@ public final class SpellbookMaker{
 		while(!feat2da.getEntry("Label", feat2daRow).equals("####START_OF_NEW_SPELLBOOK_RESERVE")){
 			feat2daRow++;
 		}
+		feat2daRow++;
 	}
 	private static void getNextFeat2daRow(){
-		while(feat2da.getEntry("Label", feat2daRow).equals("####END_OF_NEW_SPELLBOOK_RESERVE")){
-			feat2daRow++;
+		feat2daRow++;
+		if(feat2da.getEntry("Label", feat2daRow).equals("####END_OF_NEW_SPELLBOOK_RESERVE")){
+			getFirstFeat2daRow();
 		}
 	}
 
-	private static void getFirstIPRPFeat2daRow(){
+	private static void getFirstIPRPFeats2daRow(){
 		while(!iprp_feats2da.getEntry("Label", iprp_feats2daRow).equals("####START_OF_NEW_SPELLBOOK_RESERVE")){
 			iprp_feats2daRow++;
 		}
+		iprp_feats2daRow++;
 	}
-	private static void getNextIPRPFeat2daRow(){
-		while(iprp_feats2da.getEntry("Label", iprp_feats2daRow).equals("####END_OF_NEW_SPELLBOOK_RESERVE")){
-			iprp_feats2daRow++;
+	private static void getNextIPRPFeats2daRow(){
+		iprp_feats2daRow++;
+		if(iprp_feats2da.getEntry("Label", iprp_feats2daRow).equals("####END_OF_NEW_SPELLBOOK_RESERVE")){
+			getFirstIPRPFeats2daRow();
 		}
 	}
 
-	private static void getFirsttlkRow(){
+	private static void getFirstTlkRow(){
 		while(!customtlk.getEntry(tlkRow).equals("####START_OF_NEW_SPELLBOOK_RESERVE")){
 			tlkRow++;
 		}
+		tlkRow++;
 	}
-	private static void getNexttlkRow(){
-		while(customtlk.getEntry(tlkRow).equals("####END_OF_NEW_SPELLBOOK_RESERVE")){
-			tlkRow++;
+	private static void getNextTlkRow(){
+		tlkRow++;
+		if(customtlk.getEntry(tlkRow).equals("####END_OF_NEW_SPELLBOOK_RESERVE")){
+			getFirstTlkRow();
 		}
 	}
 
 	private static String getCheckedTlkEntry(int entryNo){
-		if(entryNo > 16777216){
+		if(entryNo > MAGIC_TLK){
 			return customtlk.getEntry(entryNo-16777216);
 		}
 		return dialogtlk.getEntry(entryNo);

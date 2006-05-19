ddea22ccdc61689a5dae33c2162c34b5624ece90
primogenitor <primogenitor>
2006-05-19 18:47:55 +0000

    Updated (un)packing code

--- rmg/NWAngband/tmp_repack.bat
+++ rmg/NWAngband/tmp_repack.bat
@@ -1,13 +1,24 @@
 mkdir gfftemp
 
-@java -cp "C:\NeverwinterNights\prc\rmg\tools\modpacker\nwn-tools.jar" org.progeeks.nwn.XmlToGff gfftemp mod_nwangband/*
-@java -cp "C:\NeverwinterNights\prc\rmg\tools\modpacker\nwn-tools.jar" org.progeeks.nwn.ModPacker gfftemp nwangband.mod
+@java -cp "C:\Documents and Settings\user\Desktop\PRC Stuff\rmg\tools\modpacker\nwn-tools.jar" org.progeeks.nwn.XmlToGff gfftemp mod_nwangband/*
+copy ..\tools\nwnnsscomp.exe gfftemp\nwnnsscomp.exe
+cd gfftemp
+nwnnsscomp -g -i "C:\Documents and Settings\user\Desktop\PRC Stuff\prc\include" *.nss
+del nwnnsscomp.exe
+cd..
+@java -cp "C:\Documents and Settings\user\Desktop\PRC Stuff\rmg\tools\modpacker\nwn-tools.jar" org.progeeks.nwn.ModPacker gfftemp nwangband.mod
 del /q gfftemp\*.*
-@java -cp "C:\NeverwinterNights\prc\rmg\tools\modpacker\nwn-tools.jar" org.progeeks.nwn.XmlToGff gfftemp hak_nwangband/*
-@java -cp "C:\NeverwinterNights\prc\rmg\tools\modpacker\nwn-tools.jar" org.progeeks.nwn.ModPacker gfftemp nwangband.hak
-del /q gfftemp\*.*
+copy nwangband.mod "C:\Games\NeverwinterNights\NWN\modules\nwangband.mod"
 
-copy nwangband.mod "c:\NeverwinterNights\NWN\modules\nwangband.mod"
-copy nwangband.hak "c:\NeverwinterNights\NWN\hak\nwangband.hak"
+@java -cp "C:\Documents and Settings\user\Desktop\PRC Stuff\rmg\tools\modpacker\nwn-tools.jar" org.progeeks.nwn.XmlToGff gfftemp hak_nwangband/*
+copy ..\tools\nwnnsscomp.exe gfftemp\nwnnsscomp.exe
+cd gfftemp
+nwnnsscomp -g -i "C:\Documents and Settings\user\Desktop\PRC Stuff\prc\include" *.nss
+del nwnnsscomp.exe
+cd..
+@java -cp "C:\Documents and Settings\user\Desktop\PRC Stuff\rmg\tools\modpacker\nwn-tools.jar" org.progeeks.nwn.ModPacker gfftemp nwangband.hak
+del /q gfftemp\*.*
+copy nwangband.hak "C:\Games\NeverwinterNights\NWN\hak\nwangband.hak"
+
 del /q gfftemp
 pause
--- rmg/NWAngband/tmp_unpack.bat
+++ rmg/NWAngband/tmp_unpack.bat
@@ -1,5 +1,18 @@
-copy "c:\NeverwinterNights\NWN\modules\nwangband.mod" nwangband.mod 
-copy "c:\NeverwinterNights\NWN\hak\nwangband.hak"     nwangband.hak
-@java -cp "C:\NeverwinterNights\prc\rmg\tools\modpacker\nwn-tools.jar" org.progeeks.nwn.ModReader nwangband.mod mod_nwangband
-@java -cp "C:\NeverwinterNights\prc\rmg\tools\modpacker\nwn-tools.jar" org.progeeks.nwn.ModReader nwangband.hak hak_nwangband
+mkdir mod_nwangband
+mkdir hak_nwangband
+mkdir xmltemp
+
+copy "C:\Games\NeverwinterNights\NWN\modules\nwangband.mod" nwangband.mod
+@java -cp "C:\Documents and Settings\user\Desktop\PRC Stuff\rmg\tools\modpacker\nwn-tools.jar" org.progeeks.nwn.ModReader nwangband.mod xmltemp
+@java -cp "C:\Documents and Settings\user\Desktop\PRC Stuff\rmg\tools\modpacker\nwn-tools.jar" org.progeeks.nwn.GffToXml mod_nwangband xmltemp/*  
+del /q mod_nwangband\*.ncs
+del /q xmltemp\*.*
+
+copy "C:\Games\NeverwinterNights\NWN\hak\nwangband.hak"     nwangband.hak
+@java -cp "C:\Documents and Settings\user\Desktop\PRC Stuff\rmg\tools\modpacker\nwn-tools.jar" org.progeeks.nwn.ModReader nwangband.hak xmltemp
+@java -cp "C:\Documents and Settings\user\Desktop\PRC Stuff\rmg\tools\modpacker\nwn-tools.jar" org.progeeks.nwn.GffToXml hak_nwangband xmltemp/*
+del /q hak_nwangband\*.ncs
+del /q xmltemp\*.*
+
+del /q xmltemp
 pause

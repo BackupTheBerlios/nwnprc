30b2e739e882881071f879f2db69a95daccce41e
ornedan <ornedan>
2006-05-30 18:08:33 +0000

    Machine-specific files

--- nwnprc/tools/modpacker/ModUnpacker.cmd
+++
@@ -1,1 +0,0 @@
-@java -cp "C:\Documents and Settings\user\Desktop\PRC Stuff\prc\tools\modpacker\nwn-tools.jar" org.progeeks.nwn.ModReader %1 %2
--- nwnprc/tools/modpacker/setpath.cmd
+++
@@ -1,1 +0,0 @@
-set "PATH=%PATH%;C:\Documents and Settings\user\Desktop\PRC Stuff\prc\tools\modpacker"

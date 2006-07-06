34bf3b33009d41db09bcdf7635b69981b358391c
primogenitor <primogenitor>
2006-07-06 14:06:51 +0000

    Linked it to the xml->tlk file tool to make sure tlk is up to date

--- DocGen/copyandbuild.bat
+++ DocGen/copyandbuild.bat
@@ -1,6 +1,9 @@
+mkdir 2da
+mkdir tlk
 copy ..\prc\2das\*.2da 2da\*.2da
 copy ..\prc\race2das\*.2da 2da\*.2da
 copy ..\prc\craft2das\*.2da 2da\*.2da
+..\prc\tools\xml2tlk.exe ..\prc\tlk\prc_consortium.tlk.xml ..\prc\tlk\prc_consortium.tlk
 copy ..\prc\tlk\*.tlk tlk\*.tlk
 xcopy /iey "Main Manual Files" manual
 

d1b56f47febbc684c9d4633a6b670f8f79102bf1
primogenitor <primogenitor>
2006-07-20 06:51:48 +0000

    Remerged 2das Layed out CEP sewer placeable appearances Added effiencies to unpack batch

--- rmg/NWAngband/tmp_unpack.bat
+++ rmg/NWAngband/tmp_unpack.bat
@@ -8,15 +8,17 @@ copy "C:\Games\NeverwinterNights\NWN\modules\nwangband.mod" nwangband.mod
 del /q mod_nwangband\*.ncs
 del /q xmltemp\*.*
 
-copy "C:\Games\NeverwinterNights\NWN\hak\nwangband.hak"     nwangband.hak
-@java -cp "C:\Documents and Settings\user\Desktop\PRC Stuff\rmg\tools\modpacker\nwn-tools.jar" org.progeeks.nwn.ModReader nwangband.hak xmltemp
-@java -cp "C:\Documents and Settings\user\Desktop\PRC Stuff\rmg\tools\modpacker\nwn-tools.jar" org.progeeks.nwn.GffToXml hak_nwangband xmltemp/*
-del /q hak_nwangband\*.ncs
-del /q xmltemp\*.*
+REM No need to unpack the hak!
+REM copy "C:\Games\NeverwinterNights\NWN\hak\nwangband.hak"     nwangband.hak
+REM @java -cp "C:\Documents and Settings\user\Desktop\PRC Stuff\rmg\tools\modpacker\nwn-tools.jar" org.progeeks.nwn.ModReader nwangband.hak xmltemp
+REM @java -cp "C:\Documents and Settings\user\Desktop\PRC Stuff\rmg\tools\modpacker\nwn-tools.jar" org.progeeks.nwn.GffToXml hak_nwangband xmltemp/*
+REM del /q hak_nwangband\*.ncs
+REM del /q xmltemp\*.*
 
 del /q xmltemp
 
-copy "C:\Games\NeverwinterNights\NWN\tlk\nwangband.tlk"     nwangband.tlk
-..\tools\tlktools\tlk2xml nwangband.tlk nwangband.xml
+REM No need to unpack the tlk!
+REM copy "C:\Games\NeverwinterNights\NWN\tlk\nwangband.tlk"     nwangband.tlk
+REM ..\tools\tlktools\tlk2xml nwangband.tlk nwangband.xml
 
 pause

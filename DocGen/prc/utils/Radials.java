639dfc931ef200ca69dc420a71f2c40f56758015
ornedan <ornedan>
2006-09-24 18:58:59 +0000

    Comments

--- DocGen/prc/utils/Radials.java
+++ DocGen/prc/utils/Radials.java
@@ -1,6 +1,17 @@
 package prc.utils;
 
+/**
+ * A class for generating subradial FeatID values when given a feat.2da index and
+ * the first subradial value.
+ * 
+ * @author Ornedan
+ */
 public class Radials{
+	/**
+	 * Main method
+	 * 
+	 * @param args The program arguments
+	 */
 	public static void main(String[] args){
 		if(args.length == 0)
 			readMe();

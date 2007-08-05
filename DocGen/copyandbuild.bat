0cc49f381b3bc5dca64b4d2ca7e4e679e4e42e14
fluffyamoeba <fluffyamoeba>
2007-08-05 21:35:21 +0000

    *** empty log message ***

--- DocGen/copyandbuild.bat
+++ DocGen/copyandbuild.bat
@@ -1,10 +1,10 @@
 mkdir 2da
 mkdir tlk
-copy ..\prc\2das\*.2da 2da\*.2da
-copy ..\prc\race2das\*.2da 2da\*.2da
-copy ..\prc\craft2das\*.2da 2da\*.2da
-..\prc\tools\xml2tlk.exe ..\prc\tlk\prc_consortium.tlk.xml ..\prc\tlk\prc_consortium.tlk
-copy ..\prc\tlk\*.tlk tlk\*.tlk
+copy ..\nwnprc\2das\*.2da 2da\*.2da
+copy ..\nwnprc\race2das\*.2da 2da\*.2da
+copy ..\nwnprc\craft2das\*.2da 2da\*.2da
+..\nwnprc\tools\xml2tlk.exe ..\nwnprc\tlk\prc_consortium.tlk.xml ..\nwnprc\tlk\prc_consortium.tlk
+copy ..\nwnprc\tlk\*.tlk tlk\*.tlk
 xcopy /iey "Main Manual Files" manual
 
 nmake -NOLOGO
